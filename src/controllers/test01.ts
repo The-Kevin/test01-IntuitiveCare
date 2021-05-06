3cb2548468e1397829d9d390d133fdbeabfbbcf2
Kevin Almeida <kevin.almeida1111.ka@gmail.com>
2021-05-06 17:53:08 -0300

    finalização do test01

--- src/controllers/test01.ts
+++ src/controllers/test01.ts
@@ -6,12 +6,11 @@ import fs from "fs";
 import { Response, Request } from "express";
 
 export const test01 = async (req: Request, res: Response) => {
-  const url =
-    "http://www.ans.gov.br/prestadores/tiss-troca-de-informacao-de-saude-suplementar";
-
+  const URL_SERVER = "http://www.ans.gov.br";
+  const url = "/prestadores/tiss-troca-de-informacao-de-saude-suplementar";
   const value = [];
   try {
-    const { data } = await axios.get(url);
+    const { data } = await axios.get(URL_SERVER + url);
     const $ = cheerio.load(data);
     const queryPrimary = $(".alert-icolink:first")
       .children()
@@ -20,29 +19,22 @@ export const test01 = async (req: Request, res: Response) => {
 
     // ^^ login primary page
 
-    const htmlTiss = await axios.get(
-      url.substr(0, 21) + queryPrimary.toString()
-    );
+    const htmlTiss = await axios.get(URL_SERVER + queryPrimary.toString());
 
     const $$ = cheerio.load(htmlTiss.data);
     const link = $$('a[class="btn btn-primary btn-sm center-block"]:first');
 
-    const getLinkTiss = $$(link).each((i, href) => {
+    $$(link).each((i, href) => {
       const lastLink = $$(href).attr("href");
       value.push({ link: lastLink });
     });
 
-    await axios({
-      method: "get",
-      url: url.substr(0, 21) + value[0]["link"],
-      responseType: "stream",
-    }).then((response) => {
-      response.data.pipe(fs.createWriteStream("./src/archives/tiss.pdf"));
-    });
-    //const request = await axios.get(url.substr(0, 21) + value[0]["link"]);
-    /*const request = http.get(url.substr(0, 21) + value[0]["link"], (response) =>
+    const urlLink = URL_SERVER + value[0]["link"];
+
+    http.get(urlLink, (response) =>
       response.pipe(fs.createWriteStream("./src/archives/tiss.pdf"))
-    );*/
+    );
+    return res.redirect(urlLink);
   } catch (error) {
     console.log(error);
     return res.status(500).send("error");
